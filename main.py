d0516af8ca2242f8ca04a0308e7b3af3183807e9
amandeepsingh <amans191@gmail.com>
2018-04-17 15:13:12 +0100

    Decision tree made?

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing, tree
 from sklearn.feature_extraction import DictVectorizer
-from sklearn import cross_validation
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as mpl
 
 encoding = 'utf-8-sig'
 
@@ -31,39 +33,70 @@ ContFrame = DataFrame.drop(CatFeatures, axis=1)
 # print(ContFrame.describe())
 # print(CatFrame.describe())
 
-CatFrame.replace('?', 'NA')
-CatFrame.fillna('NA', inplace = True )
+#converting Cat features too number format
+DataFrameDummy = pd.get_dummies(DataFrame, columns = CatFrame )
 
+# print(DataFrameDummy.head())
 
-CatDict = CatFrame.T.to_dict().values()
+DataFrame.to_csv('./data/trainingSetNew.csv', index=False)
 
-# vectorizer = DictVectorizer( sparse= False)
-# vecCatDF = vectorizer.fit_transform(CatDict)
-#
-# encoding_dictionary = vectorizer.vocabulary_
+#Cleaning Data + Decsion Tree
 
+NewData = pd.read_csv('./data/trainingSetNew.csv')
 
-vecCatDF = pd.get_dummies(CatFrame)
+target = NewData['y']
 
-newDataFrame = (( ContFrame.as_matrix(), vecCatDF ))
+target.to_csv('./targetfeat.csv', index=False)
 
-target = newDataFrame['y']
+numericfeat = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
+numDataFrame = NewData[numericfeat]
 
-print(newDataFrame)
+catDataFrame = NewData.drop(numericfeat + ['id', 'y'], axis=1)
+
+#print(catDataFrame.head())
+
+catDataFrame.replace('?', 'NA')
+catDataFrame.fillna('NA', inplace = True )
+
+vecCatDF = pd.get_dummies(catDataFrame)
+
+trainDataFrame = np.hstack((numDataFrame.as_matrix(), vecCatDF))
 
 #Creating the tree thing
 DecisionTreeModel = tree.DecisionTreeClassifier(criterion='entropy')
 
-x_train, x_test, y_train, y_test = cross_validation.train_test_split(newDataFrame, target,
-                                                                     test_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(trainDataFrame, target, test_size=0.2, random_state=0)
+
+trainDataFrame = np.hstack((numDataFrame.as_matrix(), vecCatDF))
 
 DecisionTreeModel.fit(x_train, y_train)
 
-predictions = DecisionTreeModel.predict(x_test)
+pred = DecisionTreeModel.predict(x_test)
 
 #test the accuracy
-print('Accuracy= ' + str(accuracy_score(y_test, predictions, normalize=True)))
-confusionMatrix = confusion_matrix(y_test, predictions)
+print('Accuracy= ' + str(accuracy_score(y_test, pred, normalize=True)))
+confusionMatrix = confusion_matrix(y_test, pred)
+
 print(confusionMatrix)
+
+
+mpl.matshow(confusionMatrix)
+mpl.title('Confusion matrix')
+mpl.colorbar()
+mpl.ylabel('True label')
+mpl.xlabel('Predicted label')
+mpl.show()
+
+#
+# CatDict = CatFrame.T.to_dict().values()
+#
+# # vectorizer = DictVectorizer( sparse= False)
+# # vecCatDF = vectorizer.fit_transform(CatDict)
+# #
+# # encoding_dictionary = vectorizer.vocabulary_
+#
+#
+# print(newDataFrame)
+
 
 
