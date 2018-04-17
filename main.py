9400eaa052c9d064e99c162072fbbf8b8db09ec2
amandeepsingh <amans191@gmail.com>
2018-04-17 14:41:05 +0100

    Cheeky fixes

--- main.py
+++ main.py
@@ -1,85 +1,69 @@
 import pandas as pd
-from sklearn import preprocessing
+from sklearn import preprocessing, tree
 from sklearn.feature_extraction import DictVectorizer
-
-
+from sklearn import cross_validation
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 encoding = 'utf-8-sig'
 
 pd.set_option('display.width', 5000)
 pd.set_option('display.max_columns', 60)
 
+# headersfile = open('./data/headers.txt')
+# headers = headersfile.read().split()
 
-headersfile = open('./headers.txt')
-headers = headersfile.read().split()
+headers = []
 
-DataFrame = pd.read_csv('./queries.txt', names=headers, na_values=['?'])
+with open('data/dataDescription.txt', 'r') as dd:
+    for line in dd:
+        if line[0].isdigit():
+            items = line.split(' ')
+            headers.append(items[2].strip().replace(':', ''))
 
-CatFeatures = ['id','job','marital','education','default','housing','contact','month','poutcome']
+DataFrame = pd.read_csv('./data/trainingset.txt', names=headers, na_values=['?'])
+
+CatFeatures = ['id', 'job', 'marital', 'education', 'default', 'housing', 'contact', 'month', 'poutcome', 'y']
 CatFrame = DataFrame[CatFeatures]
 
-ContFrame = DataFrame.drop(CatFeatures,axis=1)
+ContFrame = DataFrame.drop(CatFeatures, axis=1)
 
-# print(ContFrame.head())
+# print(ContFrame.describe())
+# print(CatFrame.describe())
 
-CatFrame.replace('?','NA')
-CatFrame.fillna( 'NA', inplace = True )
+CatFrame.replace('?', 'NA')
+CatFrame.fillna('NA', inplace = True )
 
 
 CatDict = CatFrame.T.to_dict().values()
 
-vectorizer = DictVectorizer( sparse= False)
-vecCatDF = vectorizer.fit_transform(CatDict)
-
-encoding_dictionary = vectorizer.vocabulary_
-
-newDataFrame = (( ContFrame.as_matrix(),vecCatDF ))
+# vectorizer = DictVectorizer( sparse= False)
+# vecCatDF = vectorizer.fit_transform(CatDict)
+#
+# encoding_dictionary = vectorizer.vocabulary_
 
 
-#
-# Location2 = r'./datadescription.txt'
-# df2 = pd.read_csv(Location2, names=['Names'])
-#
-# sk = df2.iloc[0]
-# sk1 = df2.iloc[1]
-# sk2 = df2.iloc[2]
-# sk3 = df2.iloc[3]
-# sk4 = df2.iloc[4]
-# sk5 = df2.iloc[5]
-# sk6 = df2.iloc[6]
-# sk7 = df2.iloc[7]
-# sk8 = df2.iloc[8]
-# sk9 = df2.iloc[9]
-# sk10 = df2.iloc[10]
-# sk11 = df2.iloc[11]
-# sk12 = df2.iloc[12]
-# sk13 = df2.iloc[13]
-# sk14 = df2.iloc[14]
-# sk15 = df2.iloc[15]
-# sk16 = df2.iloc[16]
-# sk17 = df2.iloc[17]
-#
-# one = sk['Names']
-# two = sk1['Names']
-# three = sk2['Names']
-# four = sk3['Names']
-# five = sk4['Names']
-# six = sk5['Names']
-# seven = sk6['Names']
-# eight = sk7['Names']
-# nine = sk8['Names']
-# ten = sk9['Names']
-# eleven = sk10['Names']
-# twelve = sk11['Names']
-# thirteen = sk12['Names']
-# fourteen = sk13['Names']
-# fifteen = sk14['Names']
-# sixteen = sk15['Names']
-# seventeen = sk16['Names']
-# eighteen = sk15['Names']
-#
-# Location = r'./queries.txt'
-# DataFrame = pd.read_csv(Location, names=[one, two, three, four, five, six, seven,eight,nine,ten, eleven, twelve, thirteen,
-#                                   fourteen, fifteen, sixteen, seventeen, eighteen])
-#
+vecCatDF = pd.get_dummies(CatFrame)
+
+newDataFrame = (( ContFrame.as_matrix(), vecCatDF ))
+
+target = newDataFrame['y']
+
 print(newDataFrame)
+
+#Creating the tree thing
+DecisionTreeModel = tree.DecisionTreeClassifier(criterion='entropy')
+
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(newDataFrame, target,
+                                                                     test_size=0.2, random_state=0)
+
+DecisionTreeModel.fit(x_train, y_train)
+
+predictions = DecisionTreeModel.predict(x_test)
+
+#test the accuracy
+print('Accuracy= ' + str(accuracy_score(y_test, predictions, normalize=True)))
+confusionMatrix = confusion_matrix(y_test, predictions)
+print(confusionMatrix)
+
+
