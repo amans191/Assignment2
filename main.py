71ae7a9965042201547734127a30b625f1481a9a
amandeepsingh <amans191@gmail.com>
2018-04-22 12:52:30 +0100

    Changes

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as mpl
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 
 encoding = 'utf-8-sig'
 
@@ -13,7 +15,6 @@ pd.set_option('display.width', 5000)
 pd.set_option('display.max_columns', 60)
 
 headers = []
-
 with open('data/dataDescription.txt', 'r') as dd:
     for line in dd:
         if line[0].isdigit():
@@ -21,79 +22,91 @@ with open('data/dataDescription.txt', 'r') as dd:
             headers.append(items[2].strip().replace(':', ''))
 
 DataFrame = pd.read_csv('./data/trainingset.txt', names=headers, na_values=['?'])
+DataFrame.head()
 
-CatFeatures = ['id', 'job', 'marital', 'education', 'default', 'housing', 'contact', 'month', 'poutcome', 'y']
+CatFeatures = ['job', 'loan', 'marital', 'education', 'default', 'housing', 'contact', 'month', 'poutcome']
 CatFrame = DataFrame[CatFeatures]
 
-ContFrame = DataFrame.drop(CatFeatures, axis=1)
+ContFrame = DataFrame.drop(CatFeatures + ['id', 'y'], axis=1)
 
-# print(ContFrame.describe())
-# print(CatFrame.describe())
+CatFrame.head()
 
-#converting Cat features too number format
-DataFrameDummy = pd.get_dummies(DataFrame, columns = CatFrame )
+#  Clean Data
+CatFrame.replace('?', 'NA')
+CatFrame.replace('unknown', 'NA')
+CatFrame.fillna('NA', inplace=True)
 
-# print(DataFrameDummy.head())
+# CatFrame.contact.unique()
 
-DataFrame.to_csv('./data/trainingSetNew.csv', index=False)
+# Ready data for classifier
+vecCatDF = pd.get_dummies(CatFrame)
+trainDataFrame = np.hstack((ContFrame.as_matrix(), vecCatDF))
 
-#Cleaning Data + Decsion Tree
+vectorizer = DictVectorizer(sparse=False)
+cat_df = CatFrame.T.to_dict().values()
+vec_cat_df = vectorizer.fit_transform(cat_df)
+train_df = np.hstack((ContFrame.as_matrix(), vec_cat_df))
 
-NewData = pd.read_csv('./data/trainingSetNew.csv')
-
-target = NewData['y']
-
-#target.to_csv('./targetfeat.csv', index=False)
-
-numericfeat = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
-numDataFrame = NewData[numericfeat]
-
-catDataFrame = NewData.drop(numericfeat + ['id', 'y'], axis=1)
-
-#print(catDataFrame.head())
-
-catDataFrame.replace('?', 'NA')
-catDataFrame.fillna('NA', inplace = True )
-
-vecCatDF = pd.get_dummies(catDataFrame)
-
-trainDataFrame = np.hstack((numDataFrame.as_matrix(), vecCatDF))
-
-#Creating the tree thing
+# Create decision tree (ID3)
 DecisionTreeModel = tree.DecisionTreeClassifier(criterion='entropy')
-
-x_train, x_test, y_train, y_test = train_test_split(trainDataFrame, target, test_size=0.2, random_state=0)
-
-trainDataFrame = np.hstack((numDataFrame.as_matrix(), vecCatDF))
+target = DataFrame['y']
+print(target[0])
+x_train, x_test, y_train, y_test = train_test_split(train_df, target, test_size=0.2, random_state=0)
 
 DecisionTreeModel.fit(x_train, y_train)
 
 pred = DecisionTreeModel.predict(x_test)
-
-#test the accuracy
 print('Accuracy= ' + str(accuracy_score(y_test, pred, normalize=True)))
-confusionMatrix = confusion_matrix(y_test, pred)
 
-print(confusionMatrix)
+# KNN
+# testing KNN for different Ks using cross-validation for test error
 
+List = list(range(1, 50))
+# getting the odds ones
+neighbours = list(filter(lambda x: x % 2 != 0, List))
 
-mpl.matshow(confusionMatrix)
-mpl.title('Confusion matrix')
-mpl.colorbar()
-mpl.ylabel('True label')
-mpl.xlabel('Predicted label')
-mpl.show()
+CV_scores = []
 
-#
-# CatDict = CatFrame.T.to_dict().values()
-#
-# # vectorizer = DictVectorizer( sparse= False)
-# # vecCatDF = vectorizer.fit_transform(CatDict)
-# #
-# # encoding_dictionary = vectorizer.vocabulary_
-#
-#
-# print(newDataFrame)
+print('before')
+for k in neighbours:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, x_train, y_train, cv=10, scoring='accuracy')
+    CV_scores.append(scores.mean())
 
+Errors = [1 - x for x in CV_scores]
 
+optimalneighbours = neighbours[Errors.index(min(Errors))]
+print("The optimal number of neighbors is %d" % optimalneighbours)
 
+KNN = KNeighborsClassifier(n_neighbors=optimalneighbours)
+knn.fit(x_train, y_train.values.ravel())
+pred = knn.predict(x_test)
+print(accuracy_score(y_test, pred))
+
+# KNN had better predictions, so going ahead with KNN
+
+QueryFrame = pd.read_csv('./data/queries.txt', names=headers, na_values=['?'])
+
+QueryCatFeatures = ['job', 'loan', 'marital', 'education', 'default', 'housing', 'contact', 'month', 'poutcome']
+QueryCatFrame = QueryFrame[CatFeatures]
+QueryContFrame = QueryFrame.drop(CatFeatures + ['id', 'y'], axis=1)
+
+querycat_df = QueryCatFrame.T.to_dict().values()
+queryvec_cat_df = vectorizer.fit_transform(querycat_df)
+query_df = np.hstack((QueryContFrame.as_matrix(), queryvec_cat_df))
+
+query_pred = knn.predict(query_df)
+
+# Making asnwer file
+
+# getting ids as array
+ids = QueryFrame['id'].ravel()
+print(len(QueryFrame['id'].ravel()))
+# filedata = np.array([ids,query_pred])
+# filedata = filedata.T
+# print(filedata)
+withquotes = ["\"" + item + "\"" for item in query_pred]
+np.savetxt('predictions.txt', np.transpose([ids, withquotes]), fmt='%.18s', delimiter=',', newline='\r\n')
+
+# print(zip(ids,query_pred))
+# np.savetxt('myfile.txt', np.transpose([query_pred, ids]))
